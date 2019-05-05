f81ff91fb0cd70dcd942f5d1875b5825615d8216
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-05 16:32:24 -0700

    Blank Slate

---
+++ rps.py
@@ -0,0 +1,23 @@
+### Create a simple rock, paper, scissors game in java script
+
+#Add Welcome messaage
+#1.1 Ask user how many games they'd like to play (best 3 out of 5 etc)
+#Ask user to select rock, paper, or scissors
+#Press 1 for Rock <br>Press 2 for Paper<br>Press 3 for Scissors");
+#Get the input from the user
+#Show them their selection
+#Have computer randomly select rock, paper, or scissors
+#Display computer selection
+#Determine outcome
+    #paper beats rock, rock beats scissors, scissors beats paper
+#Display win, lose, tie message
+#Ask if user wants to play again
+#Have option to list the rules of the game
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
