c356eb02c9cebeea7b3a165fe8fe0993fba6e9a0
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-08 00:25:22 -0700

    first version of rps

--- rps.py
+++ rps.py
@@ -1,15 +1,9 @@
 ### Create a simple rock, paper, scissors game in Python
 
-# # Create a way for the user to select their weapon of choice
-# # Create a way for the computer to select a weapon
-# # Compare the two weapons
-# # Determine if the outcome is a tie
-# #  - if tie replay
-# # Declare the winner
-# # Ask to play again
-
+#import libraries
 import random
 
+#Define Functions
 def choose_weapon_player():    
     weapon_player = 0
     while weapon_player not in [1, 2, 3]:
@@ -19,43 +13,48 @@ def choose_weapon_player():
             weapon_player = 0
         if weapon_player == 1:
             print('\nYour weapon is a mighty rock!\n')
-
-        elif weapon_player == 2: 
+        elif weapon_player == 2:
             print('\nYour weapon is a smooth piece of paper!\n') 
-
         elif weapon_player == 3:
             print('\nYour weapon is a pair of sharp scissors!\n')
-
         else:
             print('\nSorry, that\'s not available, try again.\n')
-       
+    return weapon_player
+
 def choose_weapon_computer():
     weapon_computer = random.randint(1, 3)
     if weapon_computer == 1:
-            print('Your sworn enemy\'s weapon is a rock!\n') #feature - randomize 'your sworn enemy' etc.        
-
+        print('Your sworn enemy\'s weapon is a rock!\n')        
     elif weapon_computer == 2: 
-            print('The enemy pulls out a piece of paper!\n') 
-
+        print('The enemy pulls out a piece of paper!\n') 
     elif weapon_computer == 3:
-            print('Donald Trump jumps at you with a pair of sharp scissors!\n')
-    
-    else: print('check for error')
+        print('Donald Trump jumps at you with a pair of scissors!\n')                   
+    else: 
+        print('check for error')
+    return weapon_computer
 
-def compare_weapons():
+# 1 = rock, 2 = paper, 3 = scissors
+def compare_weapons(weapon_player, weapon_computer):
     if weapon_player == weapon_computer:
-        print('It\'s a tie!')
-    elif weapon_player = 1 AND weapon_computer = 2:
-        print('Paper covers Rock\pYou lose!')
-    elif weapon_p
+        print('It\'s a tie!')           
+    elif weapon_player == 1 and weapon_computer == 2:
+      print('Paper covers Rock\n\nYou Lose!')
+    elif weapon_player == 1 and weapon_computer == 3:
+        print('Rock smashes scissors!\n\nYou Win!')
+    elif weapon_player == 2 and weapon_computer == 3:
+      print('Scissors cuts paper.\n\nYou Lose!')
+    elif weapon_player == 2 and weapon_computer == 1:
+        print('Paper covers rock!\n\nYou Win!')
+    elif weapon_player == 3 and weapon_computer == 1:
+      print('Rock smashes scissors.\n\nYou Lose!')
+    elif weapon_player == 3 and weapon_computer == 2:
+        print('Scissors cuts paper!\n\nYou Win!')
 
-
+##MAIN PROGRAM##
 def main():
-    choose_weapon_player()
-    choose_weapon_computer()
-    compare_weapons()
-
-    print('End of main\n') #test that we completed main
+    player_weapon = choose_weapon_player()
+    computer_weapon = choose_weapon_computer()
+    compare_weapons(player_weapon, computer_weapon)
 
 
 if __name__ == '__main__':
