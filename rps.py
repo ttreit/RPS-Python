d60532ac677c0c7f28d4cd58346fc2ac4a79361a
ttreit <14933277+ttreit@users.noreply.github.com>
2019-05-13 00:21:04 -0700

    Create Weapon class

--- rps.py
+++ rps.py
@@ -2,6 +2,13 @@
 
 #import libraries
 import random
+import enum
+
+#Define classes
+class Weapon(enum.Enum):
+    rock = 1
+    paper = 2
+    scissors = 3
 
 #Define Functions
 def choose_weapon_player():    
@@ -11,27 +18,22 @@ def choose_weapon_player():
             weapon_player = int(input('Press 1 for Rock \nPress 2 for Paper \nPress 3 for Scissors\n\nSelect your weapon: '))
         except Exception:
             weapon_player = 0
-        if weapon_player == 1:
-            print('\nYour weapon is a mighty rock!\n')
-        elif weapon_player == 2:
-            print('\nYour weapon is a smooth piece of paper!\n') 
-        elif weapon_player == 3:
-            print('\nYour weapon is a pair of sharp scissors!\n')
-        else:
-            print('\nSorry, that\'s not available, try again.\n')
-    return weapon_player
+    weapon_player = Weapon(weapon_player)
+    print('\nYou picked ' + weapon_player.name)
+    return weapon_player.value
 
 def choose_weapon_computer():
     weapon_computer = random.randint(1, 3)
-    if weapon_computer == 1:
+    weapon_computer = Weapon(weapon_computer)
+    # print('Your enemy chose ' + weapon_computer.name)
+    
+    if weapon_computer.value == 1:
         print('Your sworn enemy\'s weapon is a rock!\n')        
-    elif weapon_computer == 2: 
+    elif weapon_computer.value == 2: 
         print('The enemy pulls out a piece of paper!\n') 
-    elif weapon_computer == 3:
+    elif weapon_computer.value == 3:
         print('Donald Trump jumps at you with a pair of scissors!\n')                   
-    else: 
-        print('check for error')
-    return weapon_computer
+    return weapon_computer.value
 
 # 1 = rock, 2 = paper, 3 = scissors
 def compare_weapons(weapon_player, weapon_computer):
@@ -55,6 +57,7 @@ def main():
     player_weapon = choose_weapon_player()
     computer_weapon = choose_weapon_computer()
     compare_weapons(player_weapon, computer_weapon)
+    print('\n\n\n')
 
 
 if __name__ == '__main__':
